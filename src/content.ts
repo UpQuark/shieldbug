3f845ca86b8ff8168bd7e47ea40acd9805fd2705
Sam Ennis <sam.e.ennis@gmail.com>
2023-04-14 14:56:44 -0400

    Misc fixes

--- src/content.ts
+++ src/content.ts
@@ -8,6 +8,7 @@ function replaceWithCat(): void {
 }
 
 function isUrlBlocked(url: string, blockedUrls: string[]): boolean {
+  if (!url.startsWith("http://") && !url.startsWith("https://"))  url = "https://" + url;
   const hostname = new URL(url).hostname;
   const mainDomain = hostname.split('.').slice(-2).join('.');
   return blockedUrls.includes(mainDomain);
