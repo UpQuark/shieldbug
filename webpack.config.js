88dcbbc60ac36bf1acecaa0da429dd9f706e02b2
Sam Ennis <sam.e.ennis@gmail.com>
2023-04-15 23:59:31 -0400

    More shit dont work

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,15 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const CopyWebpackPlugin = require('copy-webpack-plugin');
 
 module.exports = {
   mode: "development",
+  devtool: 'source-map',
   entry: {
     popup: "./src/popup.tsx",
     settings: "./src/settings.tsx",
+    background: "./src/background.ts",
+    content: "./src/content.ts"
   },
   output: {
     path: path.join(__dirname, "dist"),
@@ -46,5 +50,11 @@ module.exports = {
       filename: "settings.html",
       chunks: ["settings"],
     }),
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: 'src/manifest.json', to: '.' },
+        { from: 'src/assets', to: 'assets' }
+      ]
+    })
   ],
 };
