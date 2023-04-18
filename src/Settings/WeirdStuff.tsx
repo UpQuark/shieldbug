80c9730efd24e2ecaaf8c783dc20b5babf0166c5
Sam Ennis <sam.e.ennis@gmail.com>
2023-04-18 01:19:32 -0400

    Badge arrangement

--- src/Settings/WeirdStuff.tsx
+++ src/Settings/WeirdStuff.tsx
@@ -1,10 +1,21 @@
 // BlockedSites.tsx
 import * as React from 'react';
+import {Badge} from "react-bootstrap";
 
 const WeirdStuff: React.FC = () => {
+	/**
+	 * Spiders
+	 * Clowns
+	 * Sharks
+	 */
 	return (
 		<div>
 			<h1>Weird stuff</h1>
+			<Badge>Imminent</Badge>
+			<p>
+				Do you want to help break your habit of compulsive browsing? Replace your block page with pictures of spiders.
+				This will help you either avoid compulsive habits or overcome your fear of spiders, but possibly not both.
+			</p>
 		</div>
 	);
 };
