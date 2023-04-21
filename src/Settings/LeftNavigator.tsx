c8e7935df2cfb13c83daf5e2f8a181cbdf4ff61a
Sam Ennis <sam.e.ennis@gmail.com>
2023-04-21 03:23:04 -0400

    About page to nowhere

--- src/Settings/LeftNavigator.tsx
+++ src/Settings/LeftNavigator.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import {Row, Col, Container, Nav} from 'react-bootstrap';
+import { Row, Col, Container, Nav } from 'react-bootstrap';
 import { BrowserRouter as Router, Route, Routes, Link, useNavigate } from 'react-router-dom';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
@@ -26,20 +26,26 @@ const LeftNavigator: React.FC<LeftNavigatorProps> = ({ initialRoute = '/blocked-
 							<img style={{ height: 40 }} src="../assets/icon-128.png" />
 							<span className="brand-name text-primary">ShieldBug</span>
 						</Link>
-						<Nav className="flex-column">
-							<Nav.Link as={Link} to="/">Block sites</Nav.Link>
-							{FeatureFlags.BlockKeywords && <Nav.Link as={Link} to="/blocked-keywords">Block keywords</Nav.Link>}
-							{FeatureFlags.BlockCategories && <Nav.Link as={Link} to="/blocked-categories">Block categories</Nav.Link>}
-							{FeatureFlags.WeirdStuff && <Nav.Link as={Link} to="/weird-stuff">Weird stuff</Nav.Link>}
-						</Nav>
+						<div className="nav-links">
+							<Nav className="flex-column">
+								<Nav.Link as={Link} to="/">Block sites</Nav.Link>
+								{FeatureFlags.BlockKeywords && <Nav.Link as={Link} to="/blocked-keywords">Block keywords</Nav.Link>}
+								{FeatureFlags.BlockCategories && <Nav.Link as={Link} to="/blocked-categories">Block categories</Nav.Link>}
+								{FeatureFlags.WeirdStuff && <Nav.Link as={Link} to="/weird-stuff">Weird stuff</Nav.Link>}
+							</Nav>
+						</div>
+						<div className="about-link">
+							<Nav.Link as={Link} to="/about">About</Nav.Link>
+						</div>
 					</Col>
-					<Col xs={9} md={10}>
-						<Container style={{padding: 24}}>
+					<Col xs={9} md={10} style={{ boxShadow: "inset 8px 0px 16px -14px rgba(0, 0, 0, 0.25)" }}>
+						<Container style={{ padding: 24 }}>
 							<Routes>
 								<Route path="*" element={<BlockedSites />} />
 								<Route path="/blocked-keywords" element={<BlockedKeywords />} />
 								<Route path="/blocked-categories" element={<BlockedCategories />} />
 								<Route path="/weird-stuff" element={<WeirdStuff />} />
+								<Route path="/about" element={<WeirdStuff />} /> {/* Make sure to create an About component */}
 							</Routes>
 						</Container>
 					</Col>
