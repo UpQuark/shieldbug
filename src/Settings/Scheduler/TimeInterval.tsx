46530a75f42ee4f0a3b94c9107fa258d5caa04eb
Sam Ennis <sam.e.ennis@gmail.com>
2023-04-23 00:34:33 -0400

    Changing days to be at interval level

--- src/Settings/Scheduler/TimeInterval.tsx
+++ src/Settings/Scheduler/TimeInterval.tsx
@@ -4,11 +4,15 @@ import { Button, ButtonGroup, Col, Container, Row } from "react-bootstrap";
 import Select from "react-select";
 
 const TimeInterval = () => {
-	const [timeIntervals, setTimeIntervals] = useState([{ start: "", end: "" }]);
-	const [selectedDays, setSelectedDays] = useState(Array(7).fill(false));
+	const [timeIntervals, setTimeIntervals] = useState([
+		{ start: "", end: "", selectedDays: Array(7).fill(false) },
+	]);
 
 	const addTimeInterval = () => {
-		setTimeIntervals([...timeIntervals, { start: "", end: "" }]);
+		setTimeIntervals([
+			...timeIntervals,
+			{ start: "", end: "", selectedDays: Array(7).fill(false) },
+		]);
 	};
 
 	const handleTimeChange = (
@@ -21,10 +25,16 @@ const TimeInterval = () => {
 		setTimeIntervals(newIntervals);
 	};
 
-	const toggleDay = (index: number): void => {
-		const newSelectedDays = [...selectedDays];
-		newSelectedDays[index] = !newSelectedDays[index];
-		setSelectedDays(newSelectedDays);
+	const toggleDay = (intervalIndex: number, dayIndex: number): void => {
+		const newIntervals = [...timeIntervals];
+		newIntervals[intervalIndex].selectedDays[dayIndex] = !newIntervals[
+			intervalIndex
+			].selectedDays[dayIndex];
+		setTimeIntervals(newIntervals);
+	};
+
+	const saveInterval = (index: number) => {
+		console.log("Saving interval: ", timeIntervals[index]);
 	};
 
 	const days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
@@ -33,9 +43,13 @@ const TimeInterval = () => {
 		const options = [];
 		for (let i = 0; i < 24; i++) {
 			for (let j = 0; j < 60; j += 15) {
-				const hour = i < 10 ? `0${i}` : i;
+				const hour24 = i < 10 ? `0${i}` : i;
 				const minute = j < 10 ? `0${j}` : j;
-				options.push({ value: `${hour}:${minute}`, label: `${hour}:${minute}` });
+				const ampm = i < 12 ? "AM" : "PM";
+				const hour12 = i % 12 === 0 ? 12 : i % 12;
+				const hour12Padded = hour12 < 10 ? `0${hour12}` : hour12;
+				const formattedTime = `${hour12Padded}:${minute} ${ampm}`;
+				options.push({ value: `${hour24}:${minute}`, label: formattedTime });
 			}
 		}
 		return options;
@@ -44,53 +58,80 @@ const TimeInterval = () => {
 	const timeOptions = generateTimeOptions();
 
 	return (
-		<Container>
+		<>
 			{timeIntervals.map((interval, index) => (
-				<Row key={index} className="mb-2">
-					<Col>
-						<Select
-							options={timeOptions}
-							value={timeOptions.find((option) => option.value === interval.start)}
-							onChange={(e) => handleTimeChange(index, "start", e.value)}
-							menuPlacement="auto"
-							menuPortalTarget={document.body}
-							styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
-						/>
-					</Col>
-					<Col xs="auto">-</Col>
-					<Col>
-						<Select
-							options={timeOptions}
-							value={timeOptions.find((option) => option.value === interval.end)}
-							onChange={(e) => handleTimeChange(index, "end", e.value)}
-							menuPlacement="auto"
-							menuPortalTarget={document.body}
-							styles={{ menuPortal: (base) => ({ ...base, zIndex: 9999 }) }}
-						/>
-					</Col>
-				</Row>
+				<Container key={index} className="mb-4">
+					<Row className="mb-2">
+						<h5>From</h5>
+						<Col>
+							<Select
+								options={timeOptions}
+								value={timeOptions.find(
+									(option) => option.value === interval.start
+								)}
+								onChange={(e) => handleTimeChange(index, "start", e.value)}
+								menuPlacement="auto"
+								menuPortalTarget={document.body}
+								styles={{
+									menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+								}}
+							/>
+						</Col>
+						<Col xs="auto">
+							<h5>to</h5>
+						</Col>
+						<Col>
+							<Select
+								options={timeOptions}
+								value={timeOptions.find(
+									(option) => option.value === interval.end
+								)}
+								onChange={(e) => handleTimeChange(index, "end", e.value)}
+								menuPlacement="auto"
+								menuPortalTarget={document.body}
+								styles={{
+									menuPortal: (base) => ({ ...base, zIndex: 9999 }),
+								}}
+							/>
+						</Col>
+
+						<Col>
+							<ButtonGroup>
+								{days.map((day, dayIndex) => (
+									<Button
+										key={dayIndex}
+										className={"text-white"}
+										variant={
+											interval.selectedDays[dayIndex] ? "primary" : "secondary"
+										}
+										onClick={() => toggleDay(index, dayIndex)}
+									>
+										{day}
+									</Button>
+								))}
+							</ButtonGroup>
+						</Col>
+					</Row>
+					<Row>
+						<Col>
+							<Button
+								className="text-white"
+								onClick={() => saveInterval(index)}
+							>
+								Save Interval
+							</Button>
+						</Col>
+					</Row>
+				</Container>
 			))}
 			<Row className="mb-3">
 				<Col>
-					<Button onClick={addTimeInterval}>Add Time Interval</Button>
+					<Button className="text-white" onClick={addTimeInterval}>
+						Add Time Interval
+					</Button>
 				</Col>
 			</Row>
-			<Row>
-				<Col>
-					<ButtonGroup>
-						{days.map((day, index) => (
-							<Button
-								key={index}
-								variant={selectedDays[index] ? "primary" : "secondary"}
-								onClick={() => toggleDay(index)}
-							>
-								{day}
-							</Button>
-						))}
-					</ButtonGroup>
-				</Col>
-			</Row>
-		</Container>
+		</>
 	);
 };
 
