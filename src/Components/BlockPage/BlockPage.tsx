f4b700864b43c443c97287dbbc077275e0ce374f
Sam Ennis <sam.e.ennis@gmail.com>
2025-04-10 18:37:39 -0400

    Making themed text work

--- src/Components/BlockPage/BlockPage.tsx
+++ src/Components/BlockPage/BlockPage.tsx
@@ -2,7 +2,8 @@ import * as React from 'react';
 import { useState, useEffect } from 'react';
 import './BlockPage.scss';
 import { getTotalBlockCount } from '../../Helpers/BlockedCounter';
-import {Typography} from "@mui/material";
+import { Typography, ThemeProvider } from "@mui/material";
+import theme from "../../../styles/MuiTheme";
 
 const BlockPage: React.FC = () => {
 	const [totalCount, setTotalCount] = useState<number>(0);
@@ -22,20 +23,22 @@ const BlockPage: React.FC = () => {
 	}, []);
 
 	return (
-		<div className="block-page-container">
-			<div className="text-center">
-				<h1>Don't Panic!</h1>
-				<p className="text-secondary">ShieldBug has blocked this website.</p>
-				<img
-					style={{width: 200, height: 200}}
-					src={chrome.runtime.getURL('assets/icon-template.png')}
-					alt="Shieldbug"
-				/>
-				<p>
-					ShieldBug has blocked <Typography sx={{fontWeight: 600}} variant="body1" color={"primary"}>{totalCount}</Typography> distractions
-				</p>
+		<ThemeProvider theme={theme}>
+			<div className="block-page-container">
+				<div className="text-center">
+					<h1>Don't Panic!</h1>
+					<p className="text-secondary">ShieldBug has blocked this website.</p>
+					<img
+						style={{width: 200, height: 200}}
+						src={chrome.runtime.getURL('assets/icon-template.png')}
+						alt="Shieldbug"
+					/>
+					<p>
+						ShieldBug has blocked <Typography component="span" sx={{fontWeight: 600, display: 'inline'}} variant="body1" color="primary">{totalCount}</Typography> distractions
+					</p>
+				</div>
 			</div>
-		</div>
+		</ThemeProvider>
 	);
 };
 
