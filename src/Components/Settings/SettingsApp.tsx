cb72205d2eba8f36d05e0741095fcefc0979c825
Sam Ennis <sam.e.ennis@gmail.com>
2025-04-16 02:45:06 -0400

    Switching left navigator to MUI and adding theme toggle

--- src/Components/Settings/SettingsApp.tsx
+++ src/Components/Settings/SettingsApp.tsx
@@ -1,9 +1,8 @@
 import * as React from 'react';
 import LeftNavigator from "./LeftNavigator/LeftNavigator";
 import theme from "../../../styles/MuiTheme";
-import {ThemeProvider} from "@mui/material";
-import {Col, Container, Nav, Row} from "react-bootstrap";
-import {BrowserRouter as Router, Link, Route, Routes} from "react-router-dom";
+import { ThemeProvider, Box, Container } from "@mui/material";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import BlockedSites from "./BlockedSites/BlockedItemsDialog";
 import BlockedKeywords from "./BlockedKeywords";
 import Scheduler from "./Scheduler/Scheduler";
@@ -15,23 +14,29 @@ const SettingsApp: React.FC = () => {
 	return (
 		<ThemeProvider theme={theme}>
 			<Router>
-				<Container fluid style={{ paddingLeft: '261px', width: '100%' }}>
-					<Row>
-						<LeftNavigator/>
-						<WelcomePopover/>
-						<Col style={{width: 'calc(100% - 261px)'}}>
-							<Container style={{padding: 24}}>
-								<Routes>
-									<Route path="*" element={<BlockedSites/>}/>
-									<Route path="/blocked-keywords" element={<BlockedKeywords/>}/>
-									<Route path="/schedule" element={<Scheduler/>}/>
-									<Route path="/commitment" element={<Commitment/>}/>
-									<Route path="/weird-stuff" element={<WeirdStuff/>}/>
-								</Routes>
-							</Container>
-						</Col>
-					</Row>
-				</Container>
+				<Box sx={{ 
+					display: 'flex', 
+					width: '100%',
+					boxSizing: 'border-box',
+					position: 'relative'
+				}}>
+					<LeftNavigator/>
+					<WelcomePopover/>
+					<Box sx={{ 
+						width: 'calc(100% - 261px)', 
+						marginLeft: '261px', 
+						padding: 3,
+						boxSizing: 'border-box'
+					}}>
+						<Routes>
+							<Route path="*" element={<BlockedSites/>}/>
+							<Route path="/blocked-keywords" element={<BlockedKeywords/>}/>
+							<Route path="/schedule" element={<Scheduler/>}/>
+							<Route path="/commitment" element={<Commitment/>}/>
+							<Route path="/weird-stuff" element={<WeirdStuff/>}/>
+						</Routes>
+					</Box>
+				</Box>
 			</Router>
 		</ThemeProvider>
 	);
--- src/Components/Settings/WeirdStuff.tsx
+++ src/Components/Settings/WeirdStuff.tsx
@@ -1,14 +1,18 @@
 import * as React from 'react';
-import { Switch, FormControlLabel, Card, CardContent, Typography, Box } from '@mui/material';
+import { Switch, FormControlLabel, Card, CardContent, Typography, Box, FormControl, InputLabel, Select, MenuItem, SelectChangeEvent } from '@mui/material';
 import { useEffect, useState } from 'react';
+
+type ThemeMode = 'light' | 'dark' | 'system';
 
 const WeirdStuff: React.FC = () => {
 	const [isDeterrentEnabled, setIsDeterrentEnabled] = useState(false);
+	const [themeMode, setThemeMode] = useState<ThemeMode>('system');
 
 	useEffect(() => {
-		// Load setting from storage
-		chrome.storage.sync.get(['isDeterrentEnabled'], (data) => {
+		// Load settings from storage
+		chrome.storage.sync.get(['isDeterrentEnabled', 'themeMode'], (data) => {
 			setIsDeterrentEnabled(data.isDeterrentEnabled || false);
+			setThemeMode((data.themeMode as ThemeMode) || 'system');
 		});
 	}, []);
 
@@ -18,11 +22,51 @@ const WeirdStuff: React.FC = () => {
 		chrome.storage.sync.set({ isDeterrentEnabled: newValue });
 	};
 
+	const handleThemeChange = (event: SelectChangeEvent) => {
+		const newThemeMode = event.target.value as ThemeMode;
+		setThemeMode(newThemeMode);
+		
+		// Save to storage
+		chrome.storage.sync.set({ themeMode: newThemeMode });
+		
+		// Apply theme immediately
+		localStorage.setItem('themeMode', newThemeMode);
+		
+		// If system, check the system preference
+		if (newThemeMode === 'system') {
+			localStorage.removeItem('themeMode');
+		}
+		
+		// Force a refresh to apply the theme
+		window.location.reload();
+	};
+
 	return (
 		<Box sx={{ p: 3 }}>
 			<Typography variant="h4" component="h1" gutterBottom>
-				Weird Stuff
+				Extra Settings
 			</Typography>
+			
+			<Card sx={{ mb: 3 }}>
+				<CardContent>
+					<Typography variant="h6" gutterBottom>Theme Settings</Typography>
+					<FormControl fullWidth sx={{ mb: 2 }}>
+						<InputLabel id="theme-mode-label">Theme Mode</InputLabel>
+						<Select
+							labelId="theme-mode-label"
+							id="theme-mode-select"
+							value={themeMode}
+							label="Theme Mode"
+							onChange={handleThemeChange}
+						>
+							<MenuItem value="system">System Default</MenuItem>
+							<MenuItem value="light">Light</MenuItem>
+							<MenuItem value="dark">Dark</MenuItem>
+						</Select>
+					</FormControl>
+				</CardContent>
+			</Card>
+
 			<Typography variant="body1" paragraph>
 				Do you want to help break your habit of compulsive browsing? Replace your block page with random deterrent images.
 				This will help you associate blocked sites with unpleasant experiences, making it easier to avoid them.
